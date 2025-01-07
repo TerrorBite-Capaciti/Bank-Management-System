223160541f349b3af23176363cad609604cc69e0
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 14:51:05 +0200

    Added implementation for CustomerServiceImpl file
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/CustomerServiceImpl.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,38 @@
 package com.capaciti.terrorbite.bank_management_application.service.impl;
 
 public class CustomerServiceImpl {
+import com.capaciti.terrorbite.bank_management_application.model.Customer;
+import com.capaciti.terrorbite.bank_management_application.repository.CustomerRepository;
+import com.capaciti.terrorbite.bank_management_application.service.CustomerService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElseThrow( () -> new RuntimeException() );
+    }
+
+    @Override
+    public Customer createNewCustomer(@NotNull Customer customer) {
+        if (!customerRepository.existsById(customer.getId())) {
+            return customerRepository.save(customer);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Customer updateCustomerDetails(long id, Customer customer) {
+        return null;
+    }
 }
