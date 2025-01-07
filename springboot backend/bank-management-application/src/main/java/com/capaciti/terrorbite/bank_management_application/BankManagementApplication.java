6b154848b3c7e2c7b4c04e942d8d538149f8b679
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 11:18:40 +0200

    Initialised Spring Boot application with necessary dependencies.
    Dependencies:
    - Spring JPA
    - SQLite
    - Spring Web
    - Lombok

---
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/BankManagementApplication.java
@@ -0,0 +1,13 @@
+package com.capaciti.terrorbite.bank_management_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementApplication.class, args);
+	}
+
+}
