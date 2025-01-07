38fe0155db856a139812d174a7f518d1f4df8a97
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 16:04:50 +0200

    Added implementation of TransactionService into TransactionServiceImpl file.
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/TransactionServiceImpl.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/TransactionServiceImpl.java
@@ -1,4 +1,51 @@
 package com.capaciti.terrorbite.bank_management_application.service.impl;
 
-public class TransactionServiceImpl {
+import com.capaciti.terrorbite.bank_management_application.model.Account;
+import com.capaciti.terrorbite.bank_management_application.model.Transaction;
+import com.capaciti.terrorbite.bank_management_application.repository.TransactionRepository;
+import com.capaciti.terrorbite.bank_management_application.service.TransactionService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Override
+    public List<Transaction> getAllTransactions(Account account) {
+        if (account.getTransactions() != null) {
+            return transactionRepository.findAll();
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Transaction getTransactionById(long id) {
+        return null;
+    }
+
+    @Override
+    public Transaction addTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public boolean existsById(long id) {
+        return transactionRepository.existsById(id);
+    }
+
+    @Override
+    public Transaction existsByAccount(Account account) {
+        return null;
+    }
+
+    @Override
+    public boolean existsByAccountAndId(Account account, long id) {
+        return false;
+    }
 }
