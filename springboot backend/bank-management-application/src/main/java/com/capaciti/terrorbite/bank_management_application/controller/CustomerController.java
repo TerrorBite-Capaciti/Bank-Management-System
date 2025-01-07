2aad8ffd1d2222940c0f6b66def5cc1be203e32a
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 16:32:19 +0200

    Added main API endpoint as well as Get and Post endpoints
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/controller/CustomerController.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/controller/CustomerController.java
@@ -1,4 +1,30 @@
 package com.capaciti.terrorbite.bank_management_application.controller;
 
+import com.capaciti.terrorbite.bank_management_application.model.Customer;
+import com.capaciti.terrorbite.bank_management_application.service.CustomerService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("{id}")
+    // e.g., http://localhost:PORT/api/customers/1
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") long customerId) {
+        return new ResponseEntity<Customer>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping("/createCustomer")
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer newCustomer) {
+        return new ResponseEntity<Customer>(customerService.createNewCustomer(newCustomer), HttpStatus.CREATED);
+    }
 }
