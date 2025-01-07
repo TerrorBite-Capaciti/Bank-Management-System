e2b3236b9ddea86a8e693c4d247a2cad71b65e51
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 15:03:33 +0200

    Added implementation of AccountService into AccountServiceImpl file.
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/AccountServiceImpl.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/impl/AccountServiceImpl.java
@@ -1,4 +1,43 @@
 package com.capaciti.terrorbite.bank_management_application.service.impl;
 
-public class AccountServiceImpl {
+import com.capaciti.terrorbite.bank_management_application.model.Account;
+import com.capaciti.terrorbite.bank_management_application.repository.AccountRepository;
+import com.capaciti.terrorbite.bank_management_application.service.AccountService;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public List<Account> getAllAccounts(long id) {
+        return accountRepository.findAll();
+    }
+
+    @Override
+    public Account getAccountById(long id) {
+        return accountRepository.findById(id).orElseThrow( () -> new RuntimeException() );
+    }
+
+    @Override
+    public Account createAccount(@NotNull Account account) {
+        if (!accountRepository.existsById(account.getId())) {
+            return accountRepository.save(account);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Account updateAccount(Account account) {
+        return null;
+    }
 }
