c57ab4cb818920ff24969e372ff7edd8cd8ca08c
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 14:09:34 +0200

    Added foundational code to CustomerService file
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/CustomerService.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/service/CustomerService.java
@@ -1,4 +1,13 @@
 package com.capaciti.terrorbite.bank_management_application.service;
 
+import com.capaciti.terrorbite.bank_management_application.model.Customer;
+import java.util.List;
+
 public interface CustomerService {
+
+    Customer getCustomerById(Long id);
+
+    Customer createNewCustomer(Customer customer);
+
+    Customer updateCustomerDetails(long id, Customer customer);
 }
