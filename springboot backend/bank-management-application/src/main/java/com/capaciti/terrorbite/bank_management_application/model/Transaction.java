ff59d0f5bcc39308cf67155187c25a57d7aea4f1
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 12:30:28 +0200

    Added foundational code to Transaction model
    Co-authored-by: Lefa Jele-Masemola <lefa030205@gmail.com>

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/model/Transaction.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/model/Transaction.java
@@ -1,4 +1,76 @@
 package com.capaciti.terrorbite.bank_management_application.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "transaction_type", nullable = false)
+    private String transactionType;
+
+    @Column(name = "amount", nullable = false)
+    private String amount;
+
+    @Column(name = "date", nullable = false)
+    private Date transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    //  Getters and Setters for service to use
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
