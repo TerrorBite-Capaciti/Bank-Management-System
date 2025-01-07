85b12c70879fc122e0fd0a7bf78b7490db246538
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-07 12:23:14 +0200

    Added foundational code to Account model

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/model/Account.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/model/Account.java
@@ -1,4 +1,98 @@
 package com.capaciti.terrorbite.bank_management_application.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "account_id", nullable = false)
+    private String accountId;
+
+    @Column(name = "account_type", nullable = false)
+    private String accountType;
+
+    @Column(name = "balance", nullable = false)
+    private String balance;
+
+    @Column(name = "opened_date", nullable = false)
+    private Date openedDate;
+
+    @ManyToOne
+    @Column(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions;
+
+    //  Getters and Setters for service to use
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public Date getOpenedDate() {
+        return openedDate;
+    }
+
+    public void setOpenedDate(Date openedDate) {
+        this.openedDate = openedDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
