385484a5156806a60090e060a80e42e50ed5510e
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-10 14:57:37 +0200

    Added account type enum and values

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/account_enum/AccountType.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/account_enum/AccountType.java
@@ -1,6 +1,18 @@
 package com.capaciti.terrorbite.bank_management_application.account_enum;
 
 public enum AccountType {
-    SAVINGS,
-    PREMIUM
+    SAVINGS {
+
+        // overriding toString() method for database
+        @Override
+        public String toString() {
+            return "savings";
+        }
+    },
+    PREMIUM {
+        @Override
+        public String toString() {
+            return "premium";
+        }
+    }
 }
