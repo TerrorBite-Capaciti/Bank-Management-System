64e4f34140c335ba5a0c711ec4bd721dd43ba17b
Nizaam Haffejee <nizaam.h@icloud.com>
2025-01-10 15:09:54 +0200

    Added DTO file to combine customer and account into 1 payload from frontend

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/data_transfer_object/CustomerWithAccountDataTransferObject.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/data_transfer_object/CustomerWithAccountDataTransferObject.java
@@ -1,4 +1,18 @@
 package com.capaciti.terrorbite.bank_management_application.data_transfer_object;
 
+import com.capaciti.terrorbite.bank_management_application.account_enum.AccountType;
+import lombok.Data;
+
+@Data
 public class CustomerWithAccountDataTransferObject {
+
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public static class AccountDataTransferObject {
+        private AccountType accountType;
+        private double balance;
+    }
 }
