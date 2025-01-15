3f5ca726bcce4596eeae48f77b2683d2ea2fbd9b
Lord-Nickelbottoms <nizaam.h@icloud.com>
2025-01-15 06:52:06 +0200

    added createTransaction method for deposit in controller

--- springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/controller/AccountController.java
+++ springboot backend/bank-management-application/src/main/java/com/capaciti/terrorbite/bank_management_application/controller/AccountController.java
@@ -4,10 +4,9 @@ import com.capaciti.terrorbite.bank_management_application.model.Account;
 import com.capaciti.terrorbite.bank_management_application.service.impl.AccountServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,15 @@ public class AccountController {
         return accountService.getAllAccounts(customerId);
     }
 
+    @PostMapping("/createAccount/{customerId}")
+    public ResponseEntity<?> createAccount(@PathVariable("customerId") Long customerId, @RequestBody Account accountToCreate) {
+        System.out.printf("\n\n%s\n\n", accountToCreate.toString());
+        try {
+            Account createdAccount = accountService.createAccount(accountToCreate);
 
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdAccount);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating account: " + e.getMessage());
+        }
+    }
 }
